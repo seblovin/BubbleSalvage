17c0e572edf87873d4159fca51758340ffdbbd98
Jose Villegas <villegasjose.gg@gmail.com>
2025-01-25 21:12:53 +0100

    Update arrow logic

--- Assets/_BubbleSalvage/Environment/Scripts/CollectibleSpawner.cs
+++ Assets/_BubbleSalvage/Environment/Scripts/CollectibleSpawner.cs
@@ -1,12 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BubbleSalvage
 {
     public class CollectibleSpawner : MonoBehaviour
     {
-        public List<GameObject> _collectiblesInstances;
+        public List<CollectibleController> _collectiblesInstances;
 
-        public List<GameObject> CollectiblesInstances => _collectiblesInstances;
+        public List<CollectibleController> CollectiblesInstances => _collectiblesInstances;
+        
+        private void Awake()
+        {
+            // subscribe to collectible controller event
+            CollectibleController.OnHeightReached.AddListener(OnHeightReached);
+        }
+
+        private void OnDestroy()
+        {
+            // unsubscribe from collectible controller event
+            CollectibleController.OnHeightReached.RemoveListener(OnHeightReached);
+        }
+
+        private void OnHeightReached(CollectibleController collectible)
+        {
+            // remove entry from collection
+            _collectiblesInstances.Remove(collectible);
+        }
     }
 }
