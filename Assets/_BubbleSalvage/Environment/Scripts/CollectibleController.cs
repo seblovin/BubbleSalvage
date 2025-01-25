53e16f796bdfd4545bc271fc12f80e992c77b75c
Jose Villegas <villegasjose.gg@gmail.com>
2025-01-25 12:58:55 +0100

    Score UI and logic

--- Assets/_BubbleSalvage/Environment/Scripts/CollectibleController.cs
+++ Assets/_BubbleSalvage/Environment/Scripts/CollectibleController.cs
@@ -1,5 +1,7 @@
-﻿using NaughtyAttributes;
+﻿using DG.Tweening;
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace BubbleSalvage
 {
@@ -7,20 +9,35 @@ namespace BubbleSalvage
     {
         [SerializeField] private CollectibleUIController _uiController;
         [SerializeField] private ConstantForce _constantForce;
-        [SerializeField] private int _collectibleScore;
+        [SerializeField] private int _score;
         [SerializeField] private GameObject _ballon;
+        [SerializeField] private float _heightToScore;
+
+        private bool _canScore = true;
+
+        public static UnityEvent<CollectibleController> OnHeightReached = new();
+
+        public int Score
+        {
+            get => _score;
+        }
+
+        public bool CanScore
+        {
+            get => _canScore;
+        }
 
         public void Raise(float force)
         {
             // change gravity to positive simulating falling down
             _constantForce.force = new Vector3(0, force, 0);
         }
-        
+
         public void AttachBalloon()
         {
             _ballon.gameObject.SetActive(true);
         }
-        
+
         public void RemoveBalloon()
         {
             _ballon.gameObject.SetActive(false);
@@ -38,6 +55,18 @@ namespace BubbleSalvage
                 Raise(15);
                 AttachBalloon();
             }
+            
+            // trigger event after object reaches a certain height
+            if (transform.position.y >= _heightToScore)
+            {
+                OnHeightReached?.Invoke(this);
+                transform.DOScale(Vector3.zero, 1f).onComplete = () => Destroy(gameObject);
+            }
+        }
+
+        public void RemoveScore()
+        {
+            _canScore = false;
         }
 
         [Button]
