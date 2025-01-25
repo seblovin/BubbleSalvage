9e051aecec94b04b7943ac97b284cf3f2bca2d22
LadaBaibak <baibaklada@gmail.com>
2025-01-25 20:06:06 +0100

    Shoot

---
+++ Assets/_BubbleSalvage/Environment/Scripts/CollectibleSpawner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BubbleSalvage
+{
+    public class CollectibleSpawner : MonoBehaviour
+    {
+        public List<GameObject> _collectiblesInstances;
+
+        public List<GameObject> CollectiblesInstances => _collectiblesInstances;
+    }
+}
