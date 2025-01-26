c2b9170ba177d604d00a84360dbb2de394cb100e
Jose Villegas <villegasjose.gg@gmail.com>
2025-01-26 14:46:04 +0100

    Fixed arrow issues, keep it to a single instance

--- Assets/_BubbleSalvage/UI/Scripts/TargetsPanelController.cs
+++ Assets/_BubbleSalvage/UI/Scripts/TargetsPanelController.cs
@@ -9,20 +9,10 @@ namespace BubbleSalvage
         public PlayerView _playerView;
         public GameObject _arrow;
         public CollectibleSpawner _collectibleSpawner;
-
-        private Dictionary<CollectibleController, GameObject> _arrows = new Dictionary<CollectibleController, GameObject>();
-
+        
         private void Start()
         {
-            // create arrow for each collectible
-
-            foreach (var collectible in _collectibleSpawner.CollectiblesInstances)
-            {
-                GameObject arrow = Instantiate(_arrow, transform);
-                // save arrow instance
-                _arrows.Add(collectible, arrow);
-            }
-            
+            _arrow.SetActive(false);
             // subscribe to collectible controller event
             CollectibleController.OnHeightReached.AddListener(OnHeightReached);
         }
@@ -35,8 +25,8 @@ namespace BubbleSalvage
 
         private void OnHeightReached(CollectibleController collectible)
         {
-            // remove from dictionary
-            _arrows.Remove(collectible);
+            // de-active arrow
+            _arrow.SetActive(false);
         }
 
         private void Update()
@@ -55,28 +45,23 @@ namespace BubbleSalvage
                     closestCollectible = collectible;
                 }
             }
-            
-            if (!_arrows.ContainsKey(closestCollectible)) return;
-            
-            // deactive all arrows
-            foreach (var arrowPair in _arrows)
+
+            if (minDistance <= _distanceLimit)
             {
-                arrowPair.Value.SetActive(false);
+                _arrow.SetActive(false);
+                return;
             }
             
-            if (minDistance <= _distanceLimit) return;
-
-            GameObject arrow = _arrows[closestCollectible];
             Vector3 screenPos = Camera.main.WorldToScreenPoint(closestCollectible.transform.position);
-            arrow.SetActive(true);
+            _arrow.SetActive(true);
             // get direction to collectible from center of screen
             Vector3 direction = screenPos - new Vector3(Screen.width / 2, Screen.height / 2, 0);
             // normalize
             direction.Normalize();
             // set arrow rotation
-            arrow.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+            _arrow.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
             // put arrow on border of screen given the direction
-            arrow.transform.position = new Vector3(Screen.width / 2, Screen.height / 2, 0) + direction * 150;
+            _arrow.transform.position = new Vector3(Screen.width / 2, Screen.height / 2, 0) + direction * 150;
         }
     }
 }
